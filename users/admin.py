4cf6164b3407e4d96017070589bfaa47b5d5a450
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-07 23:06:31 +0500

    Добавлено поле is_active для прользователя. Проведены миграции

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'last_name', 'first_name', 'pk', 'is_active')
+    list_filter = ('last_name', 'is_active')  # Добавлено поле is_active для фильтрации
+    search_fields = ('email', 'last_name', 'first_name')  # Добавлена возможность поиска
+    ordering = ('last_name', 'first_name')  # Установлена сортировка по умолчанию
