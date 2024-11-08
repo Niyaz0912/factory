5dfd1afcbc5760db1e94ad1b5bcd814179f8280b
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-11-08 17:14:21 +0300

    Расширена модель процесса.

--- process_history/models.py
+++ process_history/models.py
@@ -1,9 +1,20 @@
 from django.db import models
-from users.models import User
+
+from django.conf import settings
 
 
 class ProcessHistory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    detail_name = models.CharField(max_length=100, verbose_name="Наименование изделия")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
     parameter_name = models.CharField(max_length=100)
     value = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    detail_quantity = models.IntegerField(default=0, verbose_name="Количество деталей")
+
+    def __str__(self):
+        return f'{self.detail_name}, {self.user}'
+
+    class Meta:
+        verbose_name = 'process history'
+        verbose_name_plural = 'process histories'
