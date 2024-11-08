6e1b2620de4bc8cd642b42ee8e57f3273873213b
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-11-08 17:14:41 +0300

    Добавлены UserRoles

--- users/models.py
+++ users/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin', _('admin')
+    MASTER = 'master', _('master')
+    USER = 'user', _('user')
 
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
     phone = models.CharField(max_length=35, verbose_name='telephone_number', **NULLABLE)
     telegram = models.CharField(max_length=150, verbose_name='Telegram_username', **NULLABLE)
     is_active = models.BooleanField(default=True, verbose_name='active')
