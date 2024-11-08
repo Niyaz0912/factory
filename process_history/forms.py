3f2d5da849a7ea1bc0f287a7c5668c05fd036231
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-11-08 17:13:46 +0300

    Добавлены формы полная и сокращенная.

---
+++ process_history/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from process_history.models import ProcessHistory
+
+
+class ProcessHistoryForm(forms.ModelForm):
+    class Meta:
+        model = ProcessHistory
+        fields = "__all__"
+
+
+class ProcessHistoryFormShort(forms.ModelForm):
+    class Meta:
+        model = ProcessHistory
+        exclude = ("param3", "param4", "param5")
