6da241df3d69f55836fb2797e8a1203b9b1bbe93
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-15 21:57:13 +0500

    Обновил маршруты и шаблоны

--- process_history/urls.py
+++ process_history/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 from .views import (
-    HomePageView, ShiftAssignmentUpdateView, ShiftAssignmentDetailView, ShiftAssignmentCreateView,
-    HistoryProcessCreateView
-
+    HomePageView,
+    ShiftAssignmentUpdateView,
+    ShiftAssignmentDetailView,
+    ShiftAssignmentCreateView,
+    HistoryProcessCreateView,
+    ShiftAssignmentListView
 )
 
 app_name = 'process_history'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('shift_assignment/', ShiftAssignmentDetailView.as_view(), name='shift_assignment'),
-    path('history_process_create/<int:assignment_id>/', HistoryProcessCreateView.as_view(),
-         name='history_process_create'),
-    path('shift_assignments_update/<int:assignment_id>/', ShiftAssignmentUpdateView.as_view(),
-         name='shift_assignments_update'),
-    path('shift_assignment/create/', ShiftAssignmentCreateView.as_view(), name='shift_assignment_create'),
+    path('shift_assignment_list/', ShiftAssignmentListView.as_view(), name='shift_assignment_list'),
+    path('shift_assignment/<int:pk>/', ShiftAssignmentDetailView.as_view(), name='shift_assignment_detail'),  # Изменено
+    path('history_process_create/<int:assignment_id>/', HistoryProcessCreateView.as_view(), name='history_process_create'),  # Добавлен assignment_id
+    path('shift_assignments_update/<int:pk>/', ShiftAssignmentUpdateView.as_view(), name='shift_assignments_update'),  # Изменено на pk
+    path('shift_assignment_create_update/', ShiftAssignmentCreateView.as_view(), name='shift_assignment_create'),
 ]
