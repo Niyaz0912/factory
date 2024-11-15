691adf76e734bb6f87f3046cf4d77d0e9fdb5a6d
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-11-15 16:18:19 +0300

    Добавлены пользователи с разным уровнем доступа

--- users/management/commands/ccsu.py
+++ users/management/commands/ccsu.py
@@ -12,9 +12,38 @@ class Command(BaseCommand):
             last_name='Adminov',
             is_staff=True,
             is_superuser=True,
+            is_active=True,
+            role='admin'
+        )
+
+        user.set_password('qwerty')
+        user.save()
+        print('Admin created')
+
+        moderator = User.objects.create(
+            username='master@web.bm',
+            first_name='Master',
+            last_name='Masterov',
+            role='master',
+            is_staff=True,
+            is_superuser=False,
+            is_active=True
+        )
+
+        moderator.set_password('qwerty')
+        moderator.save()
+        print('Master created')
+
+        user = User.objects.create(
+            username='operator@web.bm',
+            first_name='Operator',
+            last_name='Operatov',
+            role='Operator',
+            is_staff=False,
+            is_superuser=False,
             is_active=True
         )
 
         user.set_password('qwerty')
         user.save()
-        return 'Admin created'
+        print('Operator created')
