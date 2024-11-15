897469a122474f21c77fbbb18f2906975b8631ef
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-11-15 16:19:13 +0300

    Добавлены урлы чтобы не ругалось

--- process_history/urls.py
+++ process_history/urls.py
@@ -1,20 +1,18 @@
 from django.urls import path
 from .views import (
-    HomePageView,
-    ShiftAssignmentView,
-    HistoryProcessCreateView,
-    CompleteProcessHistoryView,
-    ShiftAssignmentsUpdateView,
-    ShiftAssignmentCreateView,
+    HomePageView, ShiftAssignmentUpdateView, ShiftAssignmentDetailView, ShiftAssignmentCreateView,
+    HistoryProcessCreateView
+
 )
 
 app_name = 'process_history'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('shift_assignment/', ShiftAssignmentView.as_view(), name='shift_assignment'),
-    path('history_process_create/<int:assignment_id>/', HistoryProcessCreateView.as_view(), name='history_process_create'),
-    path('complete_process_history/', CompleteProcessHistoryView.as_view(), name='complete_process_history'),
-    path('shift_assignments_update/<int:assignment_id>/', ShiftAssignmentsUpdateView.as_view(), name='shift_assignments_update'),
+    path('shift_assignment/', ShiftAssignmentDetailView.as_view(), name='shift_assignment'),
+    path('history_process_create/<int:assignment_id>/', HistoryProcessCreateView.as_view(),
+         name='history_process_create'),
+    path('shift_assignments_update/<int:assignment_id>/', ShiftAssignmentUpdateView.as_view(),
+         name='shift_assignments_update'),
     path('shift_assignment/create/', ShiftAssignmentCreateView.as_view(), name='shift_assignment_create'),
 ]
