aeb9f235f297aa9dfff4b1c2ac047ca7eb8755a1
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-18 11:14:51 +0500

    Прописал админки для пользователей и истории процеса

--- process_history/admin.py
+++ process_history/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from process_history.models import ProcessHistory
 
-# Register your models here.
+
+@admin.register(ProcessHistory)
+class ProcessHistory(admin.ModelAdmin):
+    list_display = ('process_name', 'detail_name', 'user', 'machine_id',)
+    ordering = ('process_name',)
+    list_filter = ('process_name', 'user',)
--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'last_name', 'first_name', 'role', 'pk', 'is_active')
+    list_display = ('username', 'last_name', 'first_name', 'role', 'pk', 'is_active')
     list_filter = ('last_name', 'is_active')  # Добавлено поле is_active для фильтрации
-    search_fields = ('email', 'last_name', 'first_name')  # Добавлена возможность поиска
+    search_fields = ('username', 'last_name', 'first_name')  # Добавлена возможность поиска
     ordering = ('last_name', 'first_name')  # Установлена сортировка по умолчанию
