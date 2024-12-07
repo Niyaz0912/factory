397d00224d2c56df75d9e094c3959d5f49d0a5d9
Niyaz Khasanov <khasanchik85@gmail.com>
2024-12-07 21:52:07 +0500

    Изменены маршруты сменного задания

--- shift_assignment/urls.py
+++ shift_assignment/urls.py
@@ -11,9 +11,7 @@ app_name = 'shift_assignment'
 urlpatterns = [
     path('', ShiftAssignmentListView.as_view(), name='shift_assignment_list'),
     path('create/', ShiftAssignmentCreateView.as_view(), name='assignments_create'),
-    path('detail/<int:pk>/', ShiftAssignmentDetailView.as_view(), name='shift_assignment_detail'),
-    path('update/<int:pk>/', ShiftAssignmentUpdateView.as_view(), name='shift_assignment_update'),
-    path('delete/<int:pk>/', ShiftAssignmentDeleteView.as_view(), name='shift_assignment_delete'),
-
- ]
-
+    path('<int:pk>/', ShiftAssignmentDetailView.as_view(), name='shift_assignment_detail'),
+    path('<int:pk>/update/', ShiftAssignmentUpdateView.as_view(), name='assignments_update'),
+    path('<int:pk>/delete/', ShiftAssignmentDeleteView.as_view(), name='assignments_delete'),
+]
