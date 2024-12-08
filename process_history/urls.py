2d4927f0f7a4de46039aab92c1a76c82a9877db0
Niyaz Khasanov <khasanchik85@gmail.com>
2024-12-09 03:52:49 +0500

    Добавлено отображение полной информации о сменном задании. Обнавлен шаблон history_process_create.html. В представлениях добавлена логика для сохранения истории процесса (находится на стадии разработки)

--- process_history/urls.py
+++ process_history/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import (
-    HomePageView, HistoryProcessCreateView,
-)
+from .views import HistoryProcessCreateView, save_process_history, HomePageView
 
 app_name = 'process_history'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('history/create/<int:assignment_id>/', HistoryProcessCreateView.as_view(), name='history_process_create'),
-    ]
+    path('process/history/create/<int:assignment_id>/', HistoryProcessCreateView.as_view(), name='history_process_create'),
+    path('process/history/save/', save_process_history, name='save_process_history'),
+]
--- process_history/views.py
+++ process_history/views.py
@@ -2,7 +2,7 @@ from django.views.generic import CreateView, DetailView, UpdateView, ListView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.views.generic import TemplateView
-
+from django.shortcuts import render, redirect
 from shift_assignment.models import ShiftAssignment
 from .forms import ProcessHistoryForm
 from .models import ProcessHistory
@@ -32,3 +32,26 @@ class HistoryProcessCreateView(LoginRequiredMixin, CreateView):
         process_history.save()
 
         return super().form_valid(form)
+
+
+def save_process_history(request):
+    if request.method == 'POST':
+        # Обработка данных формы и сохранение в базу данных
+        # Пример:
+        control_code = request.POST.get('control_code')
+        mark = request.POST.get('mark')
+        quantity = request.POST.get('quantity')
+        parameters = request.POST.get('parameters')
+
+        # Создание объекта ProcessHistory
+        process_history = ProcessHistory(
+            control_code=control_code,
+            mark=mark,
+            quantity=quantity,
+            parameters=parameters,
+            user=request.user  # Привязка к текущему пользователю
+        )
+        process_history.save()
+
+        return redirect('some_view_name')  # Замените на правильное имя маршрута для перенаправления после сохранения
+    return render(request, 'process_history/save_process_history.html')
