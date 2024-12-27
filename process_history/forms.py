51103bc8216353ec0be33a3c588b56eabdbaa7c1
Niyaz Khasanov <khasanchik85@gmail.com>
2024-12-28 01:19:13 +0500

    Добавлены необходимые поля для параметров

--- process_history/forms.py
+++ process_history/forms.py
@@ -1,17 +1,17 @@
 from django import forms
+from django.views.generic import CreateView
+
 from process_history.models import ProcessHistory
 
 
 class ProcessHistoryForm(forms.ModelForm):
     class Meta:
         model = ProcessHistory
-        fields = "__all__"
+        fields = ['control_code', 'mark', 'quantity', 'parameters']
 
 
-class ProcessHistoryFormShort(forms.ModelForm):
-    class Meta:
-        model = ProcessHistory
-        exclude = ("param3", "param4", "param5")
-
-
-
+class HistoryProcessCreateView(CreateView):
+    model = ProcessHistory
+    form_class = ProcessHistoryForm  # Используйте свою форму
+    template_name = 'process_history/history_process_create.html'
+    success_url = '/success/'  # Укажите URL для перенаправления после успешного создания
