df2beb79d3459c08a474e2a00eacacd689da1eda
Niyaz Khasanov <khasanchik85@gmail.com>
2024-12-29 23:01:57 +0500

    Исправлены маршруты для process_history и users

--- process_history/urls.py
+++ process_history/urls.py
@@ -4,8 +4,7 @@ from .views import HomePageView, SaveProcessHistoryView, HistoryProcessCreateVie
 app_name = 'process_history'
 
 urlpatterns = [
-    path('history/create/<int:assignment_id>/', HistoryProcessCreateView.as_view(), name='history_process_create'),
-
+    path('create/<int:pk>/', HistoryProcessCreateView.as_view(), name='history_process_create'),
     path('', HomePageView.as_view(), name='home'),
     path('save-process-history/', SaveProcessHistoryView.as_view(), name='save_process_history'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,9 @@ from .views import UserRegisterView, UserLoginView, UserLogoutView, UserProfileV
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='create_user'),
-    path('login/', UserLoginView.as_view(), name='login_user'),  # Обратите внимание на имя здесь
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserRegisterView.as_view(), name='user_register'),  # Регистрация
+    path('login/', UserLoginView.as_view(), name='login_user'),  # Вход
+    path('logout/', UserLogoutView.as_view(), name='logout'),  # Выход
     path('update/<int:pk>/', UserUpdateView.as_view(), name='update_user'),  # Обновление пользователя
     path('profile/<int:pk>/', UserProfileView.as_view(), name='user_profile'),  # Профиль пользователя
     path('users/', UserListView.as_view(), name='user_list'),  # Список пользователей
