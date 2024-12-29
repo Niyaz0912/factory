50e9cd306a441ef8673c5e4dc6fe2c3e84c216b4
Niyaz Khasanov <khasanchik85@gmail.com>
2024-12-29 22:56:55 +0500

    Добавлена логика для регистрации пользователя

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404, render
 from django.views.generic import CreateView, UpdateView, ListView, DetailView
 
 from django.urls import reverse_lazy
@@ -14,7 +14,7 @@ from shift_assignment.models import ShiftAssignment
 class UserRegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
-    success_url = reverse_lazy('users:login_user')
+    success_url = reverse_lazy('users:login_user')  # Убедитесь, что это имя маршрута соответствует вашему urls.py
     template_name = 'users/register_user.html'
 
 
@@ -25,7 +25,7 @@ class UserLoginView(LoginView):
     def form_valid(self, form):
         user = form.get_user()
         login(self.request, user)
-        return redirect('users:profile_user', pk=user.pk)
+        return redirect('users:user_profile', pk=user.pk)
 
 
 class UserProfileView(LoginRequiredMixin, UpdateView):
@@ -42,7 +42,7 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
             context['shift_assignments'] = ShiftAssignment.objects.all()  # Все задания для админа и мастера
             context['can_create_shift'] = True  # Возможность создания сменного задания
         elif user_role == UserRoles.OPERATOR:
-            context['shift_assignments'] = ShiftAssignment.objects.filter(operator=self.request.user)  # Задания только для оператора
+            context['shift_assignments'] = ShiftAssignment.objects.filter(operator=self.request.user)
             context['can_create_shift'] = False  # Оператор не может создавать задания
 
         # Передаем строковые значения ролей в контекст
@@ -57,10 +57,9 @@ class UserUpdateView(UpdateView):
     model = User
     form_class = UserUpdateForm
     template_name = 'users/update_user.html'
-    success_url = reverse_lazy('users:profile_user')
 
-    def get_object(self, queryset=None):
-        return self.request.user
+    def get_success_url(self):
+        return reverse_lazy('users:user_profile', kwargs={'pk': self.object.pk})
 
 
 class UserLogoutView(LogoutView):
